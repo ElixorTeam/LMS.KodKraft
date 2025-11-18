1501b026e5f8b09173b4cd81efe44a9f4a82e59b
Daniil Aleksandrov <riosha3@gmail.com>
2025-11-18 14:51:20 +0300

    UPDATE: insert TS settings to LMS.Client

---
+++ Src/Apps/Lms/LMS.Client/wwwroot/libs/index.ts
@@ -0,0 +1,1 @@
+﻿// import './monaco/themes/one-dark-pro'
---
+++ Src/Libs/LMS.Components/Source/Utils/Css.cs
@@ -0,0 +1,18 @@
+﻿namespace LMS.Components.Source.Utils;
+
+public static class Css
+{
+    [Pure]
+    public static string Class(params string?[] classes) => Build(classes);
+
+    [Pure]
+    public static string Class(params (string?, bool)[] classes) =>
+        Build(classes.Where(x => x.Item2).Select(x => x.Item1));
+
+    [Pure]
+    public static string Class(string? always, params (string?, bool)[] classes) =>
+        Build(classes.Where(x => x.Item2).Select(x => x.Item1).Prepend(always));
+
+    private static string Build(IEnumerable<string?> items) =>
+        string.Join(" ", items).Trim();
+}
