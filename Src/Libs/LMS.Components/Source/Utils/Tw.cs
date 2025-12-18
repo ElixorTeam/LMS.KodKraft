7441a271067a84b9f534d3e806dc86bb11eeef2a
Daniil Aleksandrov <riosha3@gmail.com>
2025-12-18 12:27:12 +0300

    UPDATE: improve Accordion component

---
+++ Src/Libs/LMS.Components/Source/Utils/Tw.cs
@@ -0,0 +1,6 @@
+﻿namespace LMS.Components.Source.Utils;
+
+public static class Tw
+{
+    public const string Ring = "outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]";
+}
